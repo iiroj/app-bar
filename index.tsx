4457d3cb6bdbb2c5dd38ffa01fba7cd1f4c778bb
Iiro Jäppinen <iiro@jappinen.fi>
2018-12-26 16:19:51 +0200

    feat: add current position to state

--- index.tsx
+++ index.tsx
@@ -1,10 +1,10 @@
-import * as React from 'react';
+import * as React from "react";
 
 type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
 
 type PartialHTMLElement = Omit<
   React.HTMLProps<HTMLDivElement>,
-  'children' | 'className' | 'ref'
+  "children" | "className" | "ref"
 >;
 
 interface BaseProps extends PartialHTMLElement {
@@ -13,13 +13,16 @@ interface BaseProps extends PartialHTMLElement {
   readonly disabled?: boolean;
 }
 
+type AppBarPosition = "pinned" | "unfixed" | "hidden";
+
 type AppBarProps = BaseProps & {
-  readonly innerRef?: React.RefObject<any>;
+  readonly innerRef: React.Ref<HTMLElement>;
 };
 
 type AppBarState = {
+  position: AppBarPosition;
   scroll: number;
-  ref: React.RefObject<HTMLDivElement>;
+  ref: React.RefObject<HTMLElement>;
   top: number;
 };
 
@@ -28,15 +31,14 @@ class AppBar extends React.PureComponent<AppBarProps, AppBarState> {
     disabled: false
   };
 
-  public constructor(props: AppBarProps) {
-    super(props);
-
-    this.state = {
-      scroll: 0,
-      ref: this.props.innerRef || React.createRef(),
-      top: 0
-    };
-  }
+  public state: AppBarState = {
+    position: "unfixed",
+    scroll: 0,
+    ref:
+      (this.props.innerRef as React.RefObject<HTMLElement>) ||
+      React.createRef(),
+    top: 0
+  };
 
   private animateTop = () => {
     const ref = this.state.ref.current;
@@ -53,32 +55,35 @@ class AppBar extends React.PureComponent<AppBarProps, AppBarState> {
 
     const classList = ref.classList;
     const oldScroll = this.state.scroll;
-    const direction = scroll - oldScroll > 0 ? 'down' : 'up';
+    const direction = scroll - oldScroll > 0 ? "down" : "up";
     const newTop = this.state.top + oldScroll - scroll;
     const { height, top: fromTop } = ref.getBoundingClientRect();
 
     let top: number;
 
-    if (direction === 'down') {
+    if (direction === "down") {
       top = Math.max(newTop, -height);
-      if (!classList.contains('hidden') && newTop < -height) {
-        classList.remove('pinned');
-        classList.remove('unfixed');
-        classList.add('hidden');
+      if (!classList.contains("hidden") && newTop < -height) {
+        classList.remove("pinned");
+        classList.remove("unfixed");
+        classList.add("hidden");
+        this.setState({ position: "hidden" });
       }
     } else {
       top = Math.min(newTop, 0);
-      if (!classList.contains('pinned') && newTop > -height) {
-        classList.remove('hidden');
-        classList.remove('unfixed');
-        classList.add('pinned');
+      if (!classList.contains("pinned") && newTop > -height) {
+        classList.remove("hidden");
+        classList.remove("unfixed");
+        classList.add("pinned");
+        this.setState({ position: "pinned" });
       }
     }
 
-    if (!classList.contains('unfixed') && (fromTop > 0 || scroll === 0)) {
-      classList.remove('hidden');
-      classList.remove('pinned');
-      classList.add('unfixed');
+    if (!classList.contains("unfixed") && (fromTop > 0 || scroll === 0)) {
+      classList.remove("hidden");
+      classList.remove("pinned");
+      classList.add("unfixed");
+      this.setState({ position: "unfixed" });
     }
 
     this.setState({ scroll, top });
@@ -87,16 +92,16 @@ class AppBar extends React.PureComponent<AppBarProps, AppBarState> {
   private handleScroll = () => window.requestAnimationFrame(this.animateTop);
 
   private addEventListener() {
-    if ((typeof window as Partial<Window>) !== 'undefined') {
+    if ((typeof window as Partial<Window>) !== "undefined") {
       this.setState({ scroll: window.pageYOffset }, () =>
-        window.addEventListener('scroll', this.handleScroll)
+        window.addEventListener("scroll", this.handleScroll)
       );
     }
   }
 
   private removeEventListener() {
-    if ((typeof window as Partial<Window>) !== 'undefined') {
-      window.removeEventListener('scroll', this.handleScroll);
+    if ((typeof window as Partial<Window>) !== "undefined") {
+      window.removeEventListener("scroll", this.handleScroll);
     }
   }
 
@@ -111,9 +116,13 @@ class AppBar extends React.PureComponent<AppBarProps, AppBarState> {
       const { height, top } = ref.getBoundingClientRect();
 
       if (top >= 0) {
-        ref.classList.add('unfixed');
+        ref.classList.add("unfixed");
       } else if (top < -height) {
-        ref.classList.add('hidden');
+        ref.classList.add("hidden");
+        this.setState({ position: "hidden" });
+      } else {
+        ref.classList.add("pinned");
+        this.setState({ position: "pinned" });
       }
     }
   }
@@ -142,8 +151,8 @@ class AppBar extends React.PureComponent<AppBarProps, AppBarState> {
     const { children, innerRef, ...props } = this.props;
 
     const style: React.CSSProperties = {
-      position: 'sticky',
-      top: this.state.top + 'px'
+      position: "sticky",
+      top: this.state.top + "px"
     };
 
     return (
