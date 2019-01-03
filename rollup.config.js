5c538439e0a9ea46725518d8c21f0319a8eb1a90
Iiro Jäppinen <iiro@jappinen.fi>
2019-01-03 19:36:42 +0200

    feat: upgrade to Rollup v1.0 and build umd bundle

--- rollup.config.js
+++ rollup.config.js
@@ -1,33 +1,41 @@
-import typescript from "rollup-plugin-typescript2";
+import commonjs from 'rollup-plugin-commonjs';
 import minify from "rollup-plugin-babel-minify";
+import resolve from 'rollup-plugin-node-resolve';
+import typescript from "rollup-plugin-typescript2";
 
 import pkg from "./package.json";
 
 const production = !process.env.ROLLUP_WATCH;
 
-export default {
-  input: "src/index.tsx",
+const external = [...Object.keys(pkg.peerDependencies)]
 
-  output: [
-    {
-      file: pkg.main,
-      format: "cjs"
+const plugins = [
+  typescript({ typescript: require("typescript") }),
+  production && minify({ comments: false })
+]
+
+export default [
+  {
+    input: "src/index.tsx",
+    output: [
+      { file: pkg.main, format: "cjs" },
+      { file: pkg.module, format: "es" }
+    ],
+    external,
+    plugins
+  },
+  {
+    input: "src/index.tsx",
+    output: {
+      file: pkg.browser,
+      format: 'umd',
+      name: 'appBar'
     },
-    {
-      file: pkg.module,
-      format: "es"
-    }
-  ],
-
-  external: [...Object.keys(pkg.peerDependencies || {})],
-
-  plugins: [
-    typescript({
-      typescript: require("typescript")
-    }),
-    production &&
-      minify({
-        comments: false
-      })
-  ]
-};
+    external,
+    plugins: [
+      resolve(),
+      commonjs(),
+      ...plugins
+    ]
+  }
+];
