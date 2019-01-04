6487a6f2e5fb759ab3251b317ab10e3518085323
Iiro Jäppinen <iiro@jappinen.fi>
2019-01-04 08:47:30 +0200

    feat: use terser instead of babel-minify

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,18 @@
-import commonjs from 'rollup-plugin-commonjs';
-import minify from "rollup-plugin-babel-minify";
-import resolve from 'rollup-plugin-node-resolve';
+import commonjs from "rollup-plugin-commonjs";
+import { terser } from "rollup-plugin-terser";
+import resolve from "rollup-plugin-node-resolve";
 import typescript from "rollup-plugin-typescript2";
 
 import pkg from "./package.json";
 
 const production = !process.env.ROLLUP_WATCH;
 
-const external = [...Object.keys(pkg.peerDependencies)]
+const external = [...Object.keys(pkg.peerDependencies)];
 
 const plugins = [
   typescript({ typescript: require("typescript") }),
-  production && minify({ comments: false })
-]
+  production && terser()
+];
 
 export default [
   {
@@ -28,14 +28,10 @@ export default [
     input: "src/index.tsx",
     output: {
       file: pkg.browser,
-      format: 'umd',
-      name: 'appBar'
+      format: "umd",
+      name: "appBar"
     },
     external,
-    plugins: [
-      resolve(),
-      commonjs(),
-      ...plugins
-    ]
+    plugins: [resolve(), commonjs(), ...plugins]
   }
 ];
